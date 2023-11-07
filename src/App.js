2a9ef53240f21259e658dbd14119b7befdac7f42
knstntn <konstantin@quadrate.org>
2023-11-07 20:05:18 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2 className="text">soon</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
